9f46582dcd768eefbc3c6cc63c33ea4941bd6060
LaShante Thomas <Lrthomas3@yahoo.com>
2020-10-15 23:30:05 -0400

    Add desserts js files, survey column, and background image and nav changes

---
+++ js/desserts.js
@@ -0,0 +1,35 @@
+const desserts = [
+    {
+        
+        name: ' Lazy Boy Peach Cobbler',
+        origin: 'Southern States',
+        ingredients: '2 29oz can peaches, 1 stick butter, 2 cups sugar, 1-2 tsp lemon, 1/4 tsp cinnamon, and cornstarch mix with tsp of cup of milk',
+        batter: '1 cup of flour, 2 cups sugar, 2/3 cup milk, 1 egg, 2 tsp baking powder, vanilla extract, 1/4 tsp of salt', 
+        instructions: 'Preheat oven to 350 degrees and grease pan, mixed the peaches and other ingriendents. Mix the batter, Pour peaches into pan, 5. Pour batter on top of peaches, 6. Bake for 30 to 45 minutes',
+        photo: 'peach.jpg'
+
+    },
+
+
+    {
+        name: 'Chocolate Mousse',
+        origin: 'Louisiana',
+        ingredients: '1 cup of chocolate chips, 1 egg, 1 tsp vanilla extract, 1 cup of heavy cream',
+        batter:'Combine the chocolate chips, egg and vanilla in a blender.',
+        instructions:'Heat the cream until hot and small bubbles appear around the edge but do not boil. With the blender running, pour in the hot cream and blend until the chocolate melts and the mixture is smooth. Pour into dessert dishes, cover with plastic wrap and chill.',
+        photo: 'mousse.jpg'
+    },
+
+
+    {
+        name: 'Sweet Potato Pie',
+        origin: 'South Carolina',
+        ingredients:'1 & 1/2 cups of soft sweet potatoes, 3/4 cup sugar, 2 lg eggs, 4 tbs butter, 1/2 cup evaporated milk, 1 tsp vanilla extract, 1 tsp cinnamon, 1/4 tsp nutmeg, 8 tbsp salted butter, and 9 inch pie shell.',
+        batter: 'Mix the sweet potatoes in mixing bowl until creamy. Add in the cinnamon, nutmeg, ginger, sugar, eggs, evaporated milk, vanilla extract, and softened butter. Whisk until the mixture is nice creamy. Add sweet potatos into the pie shell. Bake the pie for 45 - 50 mins.',
+        instructions:'',
+        photo: 'pie.jpg'
+    }
+]; 
+
+
+
