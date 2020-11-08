16007cf26f95675b0ff146fa2fb801431970ba4b
LaShante Thomas <Lrthomas3@yahoo.com>
2020-11-07 23:21:51 -0500

    Add changes for survey.js, flexbox.css, index.html, and styles.css

--- js/survey.js
+++ js/survey.js
@@ -1,24 +1,23 @@
-
 let soulfood = 0;
 let creole = 0;
 
 let gullah = 0;
-let other = 0; 
+let other = 0;
 
 
 
 
 
 
-function refreshResults()  {
-     
+function refreshResults() {
+
     var results = document.getElementById('results');
 
-    results.innerHTML += '<br/>soulfood: ' + soulfood;
-    results.innerHTML += '<br/>creole: ' + creole;
-    results.innerHTML += '<br/>gullah: ' + gullah;
-    results.innerHTML += '<br/>other: ' + other;
-   
+    results.innerHTML += '<br/> You voted for soulfood: ' + soulfood;
+    results.innerHTML += '<br/> You voted for creole:  ' + creole;
+    results.innerHTML += '<br/> you voted for gullah:' + gullah;
+    results.innerHTML += '<br/> you voted for creole: ' + other;
+
     // results.innerHTML = 'total: ' + ( `${soulfood} + ${creole} + ${gullah} + ${other}`);
 
 }
@@ -26,61 +25,56 @@ function refreshResults()  {
 document.getElementById('soulfood-button').addEventListener('click', function () {
     soulfood++;
     refreshResults();
-    if (localStorage.clickcount) {
-        localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    if (localStorage.soulfood) {
+        localStorage.soulfood = Number(localStorage.soulfood) + 1;
     } else {
-        localStorage.clickcount = 1;
+        localStorage.soulfood = 1;
     }
-    document.getElementById('soulfood-button').innerHTML = 
-        localStorage.clickcount + " Total soulfood vote(s).";
-        alert("You voted for soulfood. It is good for the soul. Please try the recipes!"); 
+    document.getElementById('soulfood-button').innerHTML =
+        localStorage.soulfood + " Total participants soulfood vote(s).";
+    alert("You voted for soulfood. It is good for the soul. Please try the recipes!");
 
 });
 
 document.getElementById('creole-button').addEventListener('click', function () {
     creole++;
     refreshResults();
-    if (localStorage.clickcount) {
-        localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    if (localStorage.creole) {
+        localStorage.creole = Number(localStorage.creole) + 1;
     } else {
-        localStorage.clickcount = 1;
+        localStorage.creole = 1;
     }
     document.getElementById('creole-button').innerHTML =
-        localStorage.clickcount + " Total creole vote (s).";
-        alert("You voted for Creole. You must like spicy foods. Please try the recipes!")
-   
+        localStorage.creole + " Total participants creole vote (s).";
+    alert("You voted for Creole. You must like spicy foods. Please try the recipes!")
+
 });
 
 document.getElementById('gullah-button').addEventListener('click', function () {
     gullah++;
     refreshResults();
-    if (localStorage.clickcount) {
-        localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    if (localStorage.gullah) {
+        localStorage.gullah = Number(localStorage.gullah) + 1;
     } else {
-        localStorage.clickcount = 1;
+        localStorage.gullah = 1;
     }
     document.getElementById('gullah-button').innerHTML =
-        localStorage.clickcount + " Total gullah vote(s).";
-        alert("You voted for gullah. Please try the recipes!")
-    
+        localStorage.gullah + " Total participant gullah vote(s).";
+    alert("You voted for gullah. Please try the recipes!")
+
 });
 
 document.getElementById('other-button').addEventListener('click', function () {
     other++;
-    refreshResults( );
-    if (localStorage.clickcount) {
-        localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    refreshResults();
+    if (localStorage.other) {
+        localStorage.other = Number(localStorage.other) + 1;
     } else {
-        localStorage.clickcount = 1;
+        localStorage.other = 1;
     }
     document.getElementById('other-button').innerHTML =
-        localStorage.clickcount + " Total other vote(s).";
-        alert("Thank you for participating!");
+        localStorage.other + " Total participant other vote(s).";
+    alert("Thank you for participating!");
 
-    
+
 });
-
-
- 
-
- 
