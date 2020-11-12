1026d182d8d6b8a9a804f2b1ab4b872d4896b09f
LaShante Thomas <Lrthomas3@yahoo.com>
2020-11-12 18:20:39 -0500

    Adding code to styles.css for media queries.

--- js/survey.js
+++ js/survey.js
@@ -13,10 +13,10 @@ function refreshResults() {
 
     var results = document.getElementById('results');
 
-    results.innerHTML += '<br/> You voted for soulfood: ' + soulfood;
-    results.innerHTML += '<br/> You voted for creole:  ' + creole;
-    results.innerHTML += '<br/> you voted for gullah:' + gullah;
-    results.innerHTML += '<br/> you voted for creole: ' + other;
+    // results.innerHTML += '<br/> You voted for soulfood: ' + soulfood;
+    // results.innerHTML += '<br/> You voted for creole:  ' + creole;
+    // results.innerHTML += '<br/> you voted for gullah:' + gullah;
+    // results.innerHTML += '<br/> you voted for creole: ' + other;
 
     // results.innerHTML = 'total: ' + ( `${soulfood} + ${creole} + ${gullah} + ${other}`);
 
@@ -31,7 +31,7 @@ document.getElementById('soulfood-button').addEventListener('click', function ()
         localStorage.soulfood = 1;
     }
     document.getElementById('soulfood-button').innerHTML =
-        localStorage.soulfood + " Total participants soulfood vote(s).";
+        localStorage.soulfood + " Total visitor soulfood vote(s).";
     alert("You voted for soulfood. It is good for the soul. Please try the recipes!");
 
 });
@@ -45,7 +45,7 @@ document.getElementById('creole-button').addEventListener('click', function () {
         localStorage.creole = 1;
     }
     document.getElementById('creole-button').innerHTML =
-        localStorage.creole + " Total participants creole vote (s).";
+        localStorage.creole + " Total visitor creole vote (s).";
     alert("You voted for Creole. You must like spicy foods. Please try the recipes!")
 
 });
@@ -59,7 +59,7 @@ document.getElementById('gullah-button').addEventListener('click', function () {
         localStorage.gullah = 1;
     }
     document.getElementById('gullah-button').innerHTML =
-        localStorage.gullah + " Total participant gullah vote(s).";
+        localStorage.gullah + " Total vistor gullah vote(s).";
     alert("You voted for gullah. Please try the recipes!")
 
 });
@@ -73,7 +73,7 @@ document.getElementById('other-button').addEventListener('click', function () {
         localStorage.other = 1;
     }
     document.getElementById('other-button').innerHTML =
-        localStorage.other + " Total participant other vote(s).";
+        localStorage.other + " Total visitor other vote(s).";
     alert("Thank you for participating!");
 
 
